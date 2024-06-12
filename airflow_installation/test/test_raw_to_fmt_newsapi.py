5ca2937b5f722c9bb1a840889a8edeb0b8f850e6
margotrichez <margot.richezfr@gmail.com>
2024-06-12 19:18:15 +0200

    Create test raw to formatted newsapi

---
+++ airflow_installation/test/test_raw_to_fmt_newsapi.py
@@ -0,0 +1,8 @@
+import json
+
+from airflow_installation.dags.lib.raw_to_fmt_newsapi import *
+
+
+def test_raw_to_fmt_newsapi():
+    copy_convert_raw_to_formatted_newsapi('mock_parquet_newsapi.json')
+
