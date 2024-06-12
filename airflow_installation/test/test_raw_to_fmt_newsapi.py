fe7023475a282703e842539de8df8359082dd2cd
margotrichez <margot.richezfr@gmail.com>
2024-06-12 19:23:00 +0200

    Edit test filename raw to fmt

--- airflow_installation/test/test_raw_to_fmt_newsapi.py
+++ airflow_installation/test/test_raw_to_fmt_newsapi.py
@@ -4,5 +4,5 @@ from airflow_installation.dags.lib.raw_to_fmt_newsapi import *
 
 
 def test_raw_to_fmt_newsapi():
-    copy_convert_raw_to_formatted_newsapi('mock_parquet_newsapi.json')
+    copy_convert_raw_to_formatted_newsapi('mock_data_newsapi.json')
 
