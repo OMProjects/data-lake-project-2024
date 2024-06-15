9a5049249a7e3add54805a3ef5ac48a6c847d9df
orionvi <15966899+OrionVi1998@users.noreply.github.com>
2024-06-15 16:37:18 +0100

    Remove print

--- airflow_installation/dags/lib/raw_to_fmt_newsapi.py
+++ airflow_installation/dags/lib/raw_to_fmt_newsapi.py
@@ -24,7 +24,6 @@ def convert_raw_to_formatted_newsapi(file_name, data_entity_name, opt_date=None)
             tzinfo=timezone.utc
         )
     current_str = current.strftime("%Y%m%d")
-    print(current_str)
     TOPHEADLINES_PATH = DATALAKE_ROOT_FOLDER + f"raw/newsapi/" + data_entity_name + "/" + current_str + "/" + file_name
     FORMATTED_TOPHEADLINES_FOLDER = (DATALAKE_ROOT_FOLDER + "formatted/newsapi/" + data_entity_name + "/"
                                      + current_str + "/")
